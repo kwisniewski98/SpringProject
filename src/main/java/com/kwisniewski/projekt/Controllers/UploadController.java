2ee0997b6f078893ce59e98bcbcc5344d9b7c39c
kwisniewski98 <k.wisniewski98@gmail.com>
2020-12-05 14:15:37 +0100

    Add support for uploading files

---
+++ src/main/java/com/kwisniewski/projekt/Controllers/UploadController.java
@@ -0,0 +1,50 @@
+package com.kwisniewski.projekt.Controllers;
+
+import com.kwisniewski.projekt.Storage.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+
+@Controller
+public class UploadController {
+    private final StorageService storageService;
+
+    @Autowired
+    public UploadController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
+    @GetMapping("/files")
+    public String listUploadedFiles(Model model) throws IOException {
+        model.addAttribute("files", storageService.loadAll().toArray());
+        return "upload/uploadForm";
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+
+    @PostMapping("/files")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+
+        storageService.store(file);
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+        return "redirect:/files";
+    }
+}
